d512474b18f2c9fa3a338c4551df2cc1d274285e
tommycoleman87 <tommiilee@gmail.com>
2019-07-19 11:23:25 -0400

    added dependancies and made an axios call to swapi

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [swChars, setSwChars] = useState([]);
+  const [date, setDate] = useState([]);
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  useEffect(()=> {
+    axios.get('https://swapi.co/api/people')
+    .then(data => {
+      console.log(data);
+      setSwChars(data.data.results);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [date])
+  console.log(swChars)
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
